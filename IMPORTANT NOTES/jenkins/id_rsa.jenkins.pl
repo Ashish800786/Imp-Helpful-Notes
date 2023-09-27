5ace1d5f8a4ceb1af75f499ba38204c183da5b41
Ashish800786 <77309920+Ashish800786@users.noreply.github.com>
2023-09-27 17:07:06 +0530

    Add files via upload

---
+++ IMPORTANT NOTES/jenkins/id_rsa.jenkins.pl
@@ -0,0 +1,221 @@
+# START:  JENKINS CICD PIPELINE WITH GITHUB
+# STAGING GEOATB UI ======================================>
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAYEA4VMFvDKnRA4kjWOjVI82/RbbSdLqVPboOoUh0DulRhYkDvg3hfLX
+hmsGGUw0Fh1TaD9fXYtDtB5qVlCHGlFsUsu6RD5nllpjQWjjQIO7RmJm2Isuiqi5xHm7Zr
+JzkkFlJemaQwT9mRzE7P21RdG9HTLmHLrcJLKLQvOhQBolbzPv4VcrrKZvnv+EN8sSGE0g
+XMI/yHzkSM+fvSjXh45eARf5iDNM+yAz5sKVbNcPA9pAUmz9OlYbeHXJXzYA328JPBMoNp
+zCnFl8beDKXkWevEVszfEtb9yJTlAsU+kwON2opVovDUCJSPYec2jv5smD4mVC0uztXsD1
+iv9ZhcM35Z08DG7vFaKPDOsXRlTlzy/sWT6KM4liCah9chsqnfqF1AKAUzcDogZNYSuS+w
+cd4NPHcSmfyv3bvnbQJvSX2H9bDT+Mskw4HKSQ0h1w4vj7s/RvmQgGqjb0a86BgulZl+WC
+Emy1kKg1IZzSNiw06OT4btwctxIgxaa/MWOt765vAAAFkCAxy3UgMct1AAAAB3NzaC1yc2
+EAAAGBAOFTBbwyp0QOJI1jo1SPNv0W20nS6lT26DqFIdA7pUYWJA74N4Xy14ZrBhlMNBYd
+U2g/X12LQ7QealZQhxpRbFLLukQ+Z5ZaY0Fo40CDu0ZiZtiLLoqoucR5u2ayc5JBZSXpmk
+ME/ZkcxOz9tUXRvR0y5hy63CSyi0LzoUAaJW8z7+FXK6ymb57/hDfLEhhNIFzCP8h85EjP
+n70o14eOXgEX+YgzTPsgM+bClWzXDwPaQFJs/TpWG3h1yV82AN9vCTwTKDacwpxZfG3gyl
+5FnrxFbM3xLW/ciU5QLFPpMDjdqKVaLw1AiUj2HnNo7+bJg+JlQtLs7V7A9Yr/WYXDN+Wd
+PAxu7xWijwzrF0ZU5c8v7Fk+ijOJYgmofXIbKp36hdQCgFM3A6IGTWErkvsHHeDTx3Epn8
+r927520Cb0l9h/Ww0/jLJMOBykkNIdcOL4+7P0b5kIBqo29GvOgYLpWZflghJstZCoNSGc
+0jYsNOjk+G7cHLcSIMWmvzFjre+ubwAAAAMBAAEAAAGAAvsZMVMuOU1CmIg/6ed3H8QEpK
+ZgqT0W92c5ekq/tfym/3ezLFd5GHMkAoOqkLxUlSV5eoBUJfbdC0VHdEatey7syOhWcHvD
+omLj1r+nY5f7qlO1CuqOgpXlr8celkm9L9I2UzqmrDbgOA5nLoRGzzMULj8emHGwO/ODJ5
+jw89apDr8bec493cmdkythkua6Td/X/iwAzSHKCDOZHlb40WTizTV4iD310dCzL1xs+20Q
+usZsVzA6bs6IGnVZb5tFaqYiIdoMCq9e4a2Xs/4XUkiPGaXa0fvg6Iji90tnRLb0RZmNEM
+ao5WB1WELxM/WS0LJJCRL8i83eolZqHrcBByjQkXAaFEe1raMRcxA3JA9C+0eAYXjXLvMl
+Qww5BMDMpma/cEqJA4znYPt3553yRZxGCaKTkZoEDE94JprO3cjF41E1FX5Mv9Mq0WLDlc
+6M0MyBz9e9k1IL+K8sXMAjOImWY1lUlT7iAvMVsmHaYJU7b/yMRH1mMrJGuVMHFKcxAAAA
+wQDL738K5PP8JVbbPCCQk5rJBmbKZUTmFSSBgtPoyzwh54COCx2Llbbv2Se03SgL3IdSfs
+t+OJqK+6t/B84pCWuUq7aYhQtnJr49nB9knuGwwBgzmr2a3FFElKgc8h1I4//DHc5g+X4N
+ekxV/RKYJUC4OBH3jxWdNyQI+xEsyTaXEDzoq1xO+uwfKXLCcAEJilaGpM7sl2mn9a9TN2
+cAwGGop2ugTwhOdzyvllDPvydiL/aB1fOpovJC1E7qm/nOaOkAAADBAPL9FEAD7vcaukgZ
+iDZ5oU9gaEBKzdYAKYaVwZnuNQ1YKvuvE8DhwXIood2z4R9PCkCzjjxevNcacqLuM4AdPf
+GDNhazFNwSpa9uwkEwwJbAPmf/QS2lzzgvq8f8hJ5LNu0sBEE3KD1j36cJTf8nMJn0CZsv
++jn/LvUDMJ0Hr+B3uiPMOIaBaQ4h9ytsEKZ3K60qf+5epiCRzDtg7BWK1IEFLmttFJygca
+UB1QZJQiZpCiBeyi7kT95X6b+8/3uL2QAAAMEA7WPMgxfTIlF9Ci2a2/kS3pv4+gcViR6L
+8uX4G5slp3g1KGRXJEtjuzzi1M9+t030rhDco4p2kihNGyJvNglDbYEnQD1S4L92ZwJcEV
+ikskOsAtIipUUdXL4lK04q4LwuXNQrcya+ItA3bktYclKGW61aptiEJe8hBwKxh0e15KX9
+mbL9J4uMlXlBytbiSEcqRsRw5F8j/TUpvrS68vq6CfDiSXsSdmcPa78d0wJRabkLxOXF+M
+aQNgFZQLWe5geHAAAAFnVidW50dUBpcC0xNzItMjYtNC0xMDgBAgME
+-----END OPENSSH PRIVATE KEY-----
+
+# STAGING GEOTAB UI =========================>
+
+
+
+
+
+#PRODCUTION GEOTAB UI ==========>
+-----BEGIN OPENSSH PUBLIC KEY-----
+
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8eCF6m2lksXawSAsBL3FpCa+MeO+1qK7HNcVSFbeWxn65CLtXlF
+XdqjiqcyYVEphGRpC00ZLSr4/OObzsxTbhMy66eObicG24Xsc12DgnWBRj9qupoC61pjRrjRwgCExf/1Wgy+v+MSU9
+7sZKlsu3zNo2h2rYChgGYkKeOtDHwL8HTAo3AIW8Anevbs1aDZwwIf91NoHnmpQIfCgLD4AXUG03dVCrIG78Jlxwjb
+V8sRL6qvYKuV4xMeqkE9JrxeQSys8MCIYHvWl0arM+NAscQI99nTSsP44Ns3OGZyKoGfYJatQBdF15Hte19pDWhHOw
+wdipZhLC+iMmqM22Va+frTCeHmem0bmzs3FiZvkF8MOsVeEEDtBYn0yT7cGYU8uJylwLpY3EFXF+x96JB4MmDw7Zj1
+J7pRw7Yrh1+eQAksDfAtSZM0S2Om913CS/t5qo31ZR+gMdYOsFDvzBMw/TiUWhcbM76kewrjariD9g1TxjUFOweXvi
+bvzLozh2g1E= ubuntu@ip-172-26-4-131
+
+-----END OPENSSH PUBLIC KEY-----
+
+
+#===============================================================================
+
+-----BEGIN OPENSSH PRIVATE KEY-----
+
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAYEAvHgheptpZLF2sEgLAS9xaQmvjHjvtaiuxzXFUhW3lsZ+uQi7V5RV
+3ao4qnMmFRKYRkaQtNGS0q+Pzjm87MU24TMuunjm4nBtuF7HNdg4J1gUY/arqaAutaY0a4
+0cIAhMX/9VoMvr/jElPe7GSpbLt8zaNodq2AoYBmJCnjrQx8C/B0wKNwCFvAJ3r27NWg2c
+MCH/dTaB55qUCHwoCw+AF1BtN3VQqyBu/CZccI21fLES+qr2CrleMTHqpBPSa8XkEsrPDA
+iGB71pdGqzPjQLHECPfZ00rD+ODbNzhmciqBn2CWrUAXRdeR7XtfaQ1oRzsMHYqWYSwvoj
+JqjNtlWvn60wnh5nptG5s7NxYmb5BfDDrFXhBA7QWJ9Mk+3BmFPLicpcC6WNxBVxfsfeiQ
+eDJg8O2Y9Se6UcO2K4dfnkAJLA3wLUmTNEtjpvddwkv7eaqN9WUfoDHWDrBQ78wTMP04lF
+oXGzO+pHsK42q4g/YNU8Y1BTsHl74m78y6M4doNRAAAFkBgl+lAYJfpQAAAAB3NzaC1yc2
+EAAAGBALx4IXqbaWSxdrBICwEvcWkJr4x477Worsc1xVIVt5bGfrkIu1eUVd2qOKpzJhUS
+mEZGkLTRktKvj845vOzFNuEzLrp45uJwbbhexzXYOCdYFGP2q6mgLrWmNGuNHCAITF//Va
+DL6/4xJT3uxkqWy7fM2jaHatgKGAZiQp460MfAvwdMCjcAhbwCd69uzVoNnDAh/3U2geea
+lAh8KAsPgBdQbTd1UKsgbvwmXHCNtXyxEvqq9gq5XjEx6qQT0mvF5BLKzwwIhge9aXRqsz
+40CxxAj32dNKw/jg2zc4ZnIqgZ9glq1AF0XXke17X2kNaEc7DB2KlmEsL6IyaozbZVr5+t
+MJ4eZ6bRubOzcWJm+QXww6xV4QQO0FifTJPtwZhTy4nKXAuljcQVcX7H3okHgyYPDtmPUn
+ulHDtiuHX55ACSwN8C1JkzRLY6b3XcJL+3mqjfVlH6Ax1g6wUO/MEzD9OJRaFxszvqR7Cu
+NquIP2DVPGNQU7B5e+Ju/MujOHaDUQAAAAMBAAEAAAGBAIjMJXjJ70DgMscXM62jkwzrs9
+khOtS/YEVGOTgwfTZaU0bZGKokCANB96l4tQ6clZyEwaoNaCW2uom0jPXK29+v993vvZ5k
+fq9RVAU4MOIV447/T3BqzRyaS/g4z05M16riOXPiMCQr5d1DP/iJ5co+TbFAr4OqJ/ASSE
+pzZHFo7P9N6SgtH+TsYZUbtsADmvew5axdaQMDxyk1fRcyJAZIIU4cv/XGL+ijq3si0LV2
+DdGWX8o+7CiHE8oOfTAuzPDcCZELb8rjyL4cVpKusGY49Z4m2X5jZvzKv2fopyW2iOhI0C
+MPqLLwjfSMokhhi7BdE8/4w+X+TAwTaTKcXlQI2yoq5yv48rIWAClpnrZXceAW+yfP0vtm
+zXrNbqa1MA079MuMy4yeOnZ9gkxmgZhngrZ4N6DC/AOl4O1l9HmkoZyS2eLoFAbsngNWyY
+JX08KRXdCGCiBD21GLDSqBkhkGP2vvls5ydagl7LjuKqvwzPu5xv6WVjCBSjlutpST5QAA
+AMAlr4AVLnJ0grsG6zBCz3GqfEEt87DlLplsOhIGUUwoLL5SF38jzBB2SfalnSokyreh9R
+B29I2xaB/tt+g4eQN0mN64fduQUPoURjZBVvzgUWbdRVggX9J/zrRtr/i8OfoizZBzLXFW
+/Y4K7jtSz0GEK46X3MN/a2XISsRtkUjidqmNZT0OMYvKpUc4bQp8MaLZ1G0TxxmLWQpl0F
+EtaW4N/MJ4Hq2New0UUEdYX5MZiNyOaNi+JF12hWS2rqsNUTEAAADBAPdYreGSFOwHWZTm
+SqpoR8R20JE1V5ufwrHIWZHJKGuOSXbV0fxPj1Wbj+B1IyDoESerPgIN5h6DzQO1m6btL4
+kGg2S2ix435UzeMVMJ7ftLzCJpthGGsK5xRHT+43h/RDVx6rtOc5YgvKhjwmj+cjNdegRy
+A5svIc2B9f0Caz/ITWLApdJ7Jr+Cj6ejzc9msGVyELbpGd/Cc0RarRHq6NcOSnMTVufGuZ
+kYgDfCJ1XJOWHwKetICF5Zr3LNHScAbwAAAMEAwxAglu9wQDTbFcONnSBXOdZUNyNlBpwH
+04hQjIT3+AcvZtCG2G2+fbPfRJMMiyUoyRJy0uPCVEDEaliQHYKus8R9bfvvlzji5lIZEr
+Tl4wd4u7mjWM9qZKqGtdRuN6k5RuxwIez4V8MEYp77HrOqK20wzyLGfVaVNoc40axUDAab
+HAKv/bmtluDVmMajBLzWmMCYEcaBlp71so8yD6GMkpQL6WWxHdClCqeqpqsxcwocx9o3Gq
+Qtt3sXnNg83Rg/AAAAFnVidW50dUBpcC0xNzItMjYtNC0xMzEBAgME
+-----END OPENSSH PRIVATE KEY-----
+
+
+-----END OPENSSH PRIVATE KEY-----
+
+
+
+# /PRODCUTION GEOTAB UI ==========>
+
+# RUN COMMAND FOR GENERATING SSH KYE(id_rsa && id_rsa.pub)
+$ ssh-keygen
+
+
+#BEGINE : RUN COMMAND FRO EDIT JENKINS DEFAULT DIRECTORY 
+sudo systemctl stop jenkins
+sudo systemctl edit jenkins
+
+# paste the code in edit section
+# ------------------------------ 
+[Service]
+Environment="JENKINS_HOME=/home/ubuntu"
+
+WorkingDirectory=/home/ubuntu
+# ------------------------------ 
+
+# RUN COMMARD
+sudo systemctl start jenkins
+
+#end : RUN COMMAND FRO EDIT JENKINS DEFAULT DIRECTORY 
+
+
+
+# for creating new cicd pipline jon in jenkins git project integration
+#checkbox check GitHub project
+# add your git repository url 
+
+
+#Source Code Management
+#radio button check git
+# in Repositories section 
+# add your Repository URL with personal access token
+https://<personal_access_token>@github.com/repository_url.git
+
+# creadentials
+#add jenkins
+#kind dropdown
+#select ssh username with private key
+# create ID
+# username 
+ubuntu
+or 
+root
+
+
+# checkbox check private key
+# enter directly
+# paste your id_rsa private key
+
+
+#save with clickin add button at the last
+
+
+#credentials dropdown
+# select your id creadentials by diesciption 
+
+
+
+#set your branch for build
+# Branches to build
+*/master
+
+
+
+
+
+#for faliar of execution of apache2 service restart run commands
+sudo su    
+# visudo -f /etc/sudoers
+# nano -f /etc/sudoers
+sudo /etc
+nano sudoers
+ 
+#add add following line at the end.
+jenkins ALL= NOPASSWD: ALL
+
+
+
+# for run autmeticaly sudo service apache2 restart command by Jenkins after build 
+# need to configure in jenkins configuration 
+#Build Steps
+# add build step
+# Execute shell
+# Command
+echo password | sudo -S systemctl reload apache2.service
+echo password | sudo -S systemctl restart apache2.service
+sudo service apache2 restart
+sudo supervisorctl restart all
+
+
+# for jenkins build autometicaly when any commit changes in master branch 
+# need to configure in jenkins configuration 
+#Build Triggers
+# check the Poll SCM
+# in Schedule 
+#enter crontab----like for every min 
+* * * * *  
+
+
+# for cleanup workspace of jenkins
+#Build Environment
+#check Delete workspace before build starts
+
+
+#Post-build Actions
+#add Post-build Actions
+#select Delete workspace when build is done
+
+
+# END :  JENKINS CICD PIPELINE WITH GITHUB
